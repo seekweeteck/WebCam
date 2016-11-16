06d3bd4f7bcf04c13da7178f32ccb1604ef21f52
seekweeteck <seekweeteck@gmail.com>
2016-11-16 22:33:22 +0800

    UI updated

--- app/src/main/java/my/edu/tarc/webcam/MainActivity.java
+++ app/src/main/java/my/edu/tarc/webcam/MainActivity.java
@@ -3,43 +3,34 @@ package my.edu.tarc.webcam;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
-import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity {
-    public static final String UPLOAD_URL = "http://bait2073.esy.es/insert_student.php";
-    public static final String UPLOAD_ID = "id";
-    public static final String UPLOAD_NAME = "name";
-    public static final String UPLOAD_PHOTO = "photo";
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String UPLOAD_URL = "http://bait2073.esy.es/upload.php";
+    public static final String UPLOAD_KEY = "image";
     public static final String TAG = "MY MESSAGE";
 
-    private int REQUEST_IMAGE_CAPTURE = 1;
-    private ImageView imageViewPreview;
-    private String mCurrentPhotoPath;
+    private int PICK_IMAGE_REQUEST = 1;
 
-    private String id;
-    private String name;
-    private EditText editTextID, editTextName;
-    private Bitmap imageBitmap;
+    private Button buttonChoose, buttonView;
+    private Button buttonUpload;
+
+    private ImageView imageView;
+
+    private Bitmap bitmap;
 
     private Uri filePath;
 
@@ -47,12 +38,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editTextID = (EditText)findViewById(R.id.editTextID);
-        editTextName = (EditText)findViewById(R.id.editTextName);
-        imageViewPreview = (ImageView)findViewById(R.id.imageViewPreview);
+
+        buttonChoose = (Button) findViewById(R.id.buttonChoose);
+        buttonUpload = (Button) findViewById(R.id.buttonUpload);
+        buttonView = (Button) findViewById(R.id.buttonViewImage);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        buttonChoose.setOnClickListener(this);
+        buttonUpload.setOnClickListener(this);
     }
 
-    public String getStringImage(Bitmap bmp){
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            filePath = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getStringImage(Bitmap bmp) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
         byte[] imageBytes = baos.toByteArray();
@@ -60,8 +80,8 @@ public class MainActivity extends AppCompatActivity {
         return encodedImage;
     }
 
-    public void saveRecord(View v){
-        class UploadImage extends AsyncTask<Student,Void,String> {
+    private void uploadImage() {
+        class UploadImage extends AsyncTask<Bitmap, Void, String> {
 
             ProgressDialog loading;
             RequestHandler rh = new RequestHandler();
@@ -69,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             protected void onPreExecute() {
                 super.onPreExecute();
-                loading = ProgressDialog.show(MainActivity.this, "Uploading record", "Please wait...", true, true);
+                loading = ProgressDialog.show(MainActivity.this, "Uploading Image", "Please wait...", true, true);
             }
 
             @Override
@@ -77,115 +97,33 @@ public class MainActivity extends AppCompatActivity {
                 super.onPostExecute(s);
                 loading.dismiss();
                 Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
-                Log.d("onPostExecute", s);
             }
 
             @Override
-            protected String doInBackground(Student... params) {
-                String id = params[0].getId();
-                String name = params[0].getName();
-                String uploadImage = getStringImage(imageBitmap);
+            protected String doInBackground(Bitmap... params) {
+                Bitmap bitmap = params[0];
+                String uploadImage = getStringImage(bitmap);
 
                 HashMap<String, String> data = new HashMap<>();
-                data.put(UPLOAD_ID, id);
-                data.put(UPLOAD_NAME, name);
-                data.put(UPLOAD_PHOTO, uploadImage);
+                data.put(UPLOAD_KEY, uploadImage);
 
                 String result = rh.sendPostRequest(UPLOAD_URL, data);
-                Log.d("doInBackground", result);
+
                 return result;
             }
         }
 
         UploadImage ui = new UploadImage();
-        String id, name;
-
-        id = editTextID.getText().toString();
-        name = editTextName.getText().toString();
-        Student student = new Student(id, name, filePath);
-        ui.execute(student);
-    }
-
-    public void takePhoto(View v){
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        //Returns the first activity component that can handle the intent
-        /*if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                Log.d(getPackageName(), "Error creating photo file.");
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        getPackageName().toString(),
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-            }
-        }*/
+        ui.execute(bitmap);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-
-            //imageBitmap = (Bitmap) extras.get("data");
-            //imageViewPreview.setImageBitmap(imageBitmap);
-
-            try {
-                //imageBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
-                Bundle extras = data.getExtras();
-                imageBitmap = (Bitmap) extras.get("data");
-                imageViewPreview.setImageBitmap(imageBitmap);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            //setPic();
+    public void onClick(View v) {
+        if (v == buttonChoose) {
+            showFileChooser();
         }
-    }
-
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
-        return image;
-    }
-
-    private void setPic() {
-        // Get the dimensions of the View
-        int targetW = imageViewPreview.getWidth();
-        int targetH = imageViewPreview.getHeight();
-
-        // Get the dimensions of the bitmap
-        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-        bmOptions.inJustDecodeBounds = true;
-
-        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
-        int photoW = bmOptions.outWidth;
-        int photoH = bmOptions.outHeight;
-
-        // Determine how much to scale down the image
-        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
-
-        // Decode the image file into a Bitmap sized to fill the View
-        bmOptions.inJustDecodeBounds = false;
-        bmOptions.inSampleSize = scaleFactor;
-        bmOptions.inPurgeable = true;
-
-        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
-        imageViewPreview.setImageBitmap(bitmap);
+        if (v == buttonUpload) {
+            uploadImage();
+        }
     }
 }
